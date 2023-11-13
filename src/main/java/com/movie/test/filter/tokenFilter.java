f73b071fd9fb6db8bdf5da23569af255424b2c57
K-CHANHO <cksgh1565@gmail.com>
2023-11-13 17:30:05 +0900

    Feat: Refresh Token
    매 요청마다 토큰 리프레
    
    by. chanho 2023-11-13

--- src/main/java/com/movie/test/filter/tokenFilter.java
+++ src/main/java/com/movie/test/filter/tokenFilter.java
@@ -9,7 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 // TODO : 토크 체크 -> 모든 요청시 토큰 리프레쉬
 @Slf4j
@@ -18,6 +20,14 @@ public class tokenFilter implements Filter {
     @Autowired
     private tokenService tokenService;
 
+    private List<String> excludedUrls = new ArrayList<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        excludedUrls.add("/login");
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("--- Start Token Filter ---");
@@ -25,28 +35,32 @@ public class tokenFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        String token = req.getHeader("token");
+        String path = req.getServletPath();
+        if(!excludedUrls.contains(path)){
+            String token = req.getParameter("token");
+            if(token == null){
+                res.setStatus(403);
+                return;
+            }
 
-        Claims claims = tokenService.readJwtToken(token);
-        Date expiration = claims.getExpiration();
+            Claims claims = tokenService.readJwtToken(token);
+            Date expiration = claims.getExpiration();
 
-        // 토큰 만료일이 지난 경우 재로그인
-        if (expiration.before(new Date(new Date().getTime()))) {
-            res.setStatus(403);
+            log.info("expiration date = {}", expiration);
 
-        } else {
-            String uid = (String) claims.get("uid");
-            String type = (String) claims.get("type");
-            String newToken = tokenService.makeJwtToken(uid, type);
-            res.setHeader("token", newToken);
+            // 토큰 만료일이 지난 경우 재로그인
+            if (expiration.before(new Date(new Date().getTime()))) {
+                res.setStatus(403);
+
+            } else {
+                String uid = (String) claims.get("uid");
+                String type = (String) claims.get("type");
+                String newToken = tokenService.makeJwtToken(uid, type);
+                res.setHeader("token", newToken);
+            }
         }
+        chain.doFilter(request, response);
 
-
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
     }
 
     @Override
--- src/main/java/com/movie/test/test/controller/test.java
+++ src/main/java/com/movie/test/test/controller/test.java
@@ -1,14 +1,23 @@
 package com.movie.test.test.controller;
 
+import com.google.gson.JsonObject;
 import com.movie.test.s3.service.S3Service;
 import com.movie.test.test.dto.testDTO;
 import com.movie.test.test.service.testService;
 import com.movie.test.token.service.tokenService;
 import io.jsonwebtoken.Claims;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Controller
 public class test {
@@ -41,5 +50,18 @@ public class test {
     @ResponseBody
     public String imageUrlTest(String imageUrl){
         return s3Service.uploadImage(imageUrl);
+    }
+
+    @PostMapping("/testTokenRefresh")
+    @ResponseBody
+    public ResponseEntity testRefreshToken(String token, ServletRequest request, ServletResponse response){
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String newToken = req.getHeader("token");
+        JsonObject serverData = new JsonObject();
+        serverData.addProperty("originToken", token);
+        serverData.addProperty("newToken", newToken);
+
+        return new ResponseEntity(serverData, HttpStatus.OK);
     }
 }
