7d75aae0ad03c2a2e6783771f3df145ed8a4e5db
Chanho Kim <cksgh1565@gmail.com>
2023-11-30 23:30:16 +0900

    Feat: Add Complaint
    신고 기능
    
    by. chanho 2023-11-30

---
+++ src/main/java/com/movie/test/complaint/ComplaintEntity.java
@@ -0,0 +1,4 @@
+package com.movie.test.complaint;
+
+public class ComplaintEntity {
+}
--- src/main/java/com/movie/test/test/controller/test.java
+++ src/main/java/com/movie/test/test/controller/test.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,9 +30,14 @@ public class test {
     @Autowired
     private S3Service s3Service;
 
+    @GetMapping("/test")
+    public String index(){
+        return "index";
+    }
+
     @PostMapping("/test")
     @ResponseBody
-    public testDTO test(testDTO param){
+    public testDTO test(testDTO param) {
 
         testService.insert(param);
 
