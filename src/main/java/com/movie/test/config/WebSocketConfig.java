7e6d3ed97f0438647a008692fc4a59bb0f895cae
K-CHANHO <cksgh1565@gmail.com>
2023-12-05 14:09:26 +0900

    Feat: Add Websocket
    실시간 알람 구현을 위한 웹소켓 추가
    
    by. chanho 2023-12-05

---
+++ src/main/java/com/movie/test/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.movie.test.config;
+
+import com.movie.test.websocket.MyWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/websocket-endpoint");
+    }
+
+    @Bean
+    public WebSocketHandler myHandler(){
+        return new MyWebSocketHandler();
+    }
+}
