0abec4b0d82085350003963be3e08bcd10ca0dca
K-CHANHO <cksgh1565@gmail.com>
2023-12-05 17:28:15 +0900

    전체 커밋 202312051728

--- src/main/java/com/movie/test/complaint/ComplaintEntity.java
+++ src/main/java/com/movie/test/complaint/ComplaintEntity.java
@@ -1,9 +1,21 @@
 package com.movie.test.complaint;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "complaint")
 public class ComplaintEntity {
+
+    @Id
+    private String complaintId;
+
+    @Column
+    private String complaintUser;
+
+    @Column
+    private String getComplaintReportId;
+
 }
