b6580e4d9713ae5632d6abe19a85bfae39466778
K-CHANHO <cksgh1565@gmail.com>
2024-02-02 17:08:56 +0900

    Feat: Add Bookmark Controller
    북마크 컨트롤러 추가
    
    by. chanho 2024-02-02

--- src/main/java/com/movie/test/report/bookmark/service/BookmarkService.java
+++ src/main/java/com/movie/test/report/bookmark/service/BookmarkService.java
@@ -1,10 +1,14 @@
 package com.movie.test.report.bookmark.service;
 
 import com.movie.test.report.bookmark.dto.BookmarkDTO;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface BookmarkService {
 
     BookmarkDTO registBookmark(BookmarkDTO bookmarkDTO);
     void deleteBookmark(Long bookmarkId);
+
+    Slice<BookmarkDTO> getBookmarkList(BookmarkDTO bookmarkDTO, Pageable pageable);
 
 }
