f08ccb4b93f4ac3eddbb1e6fedc3b6546aad1a89
K-CHANHO <cksgh1565@gmail.com>
2024-02-14 11:46:55 +0900

    Refactor: Add Swagger Description
    북마크 관련 스웨거 내용 추가
    
    by. chanho 2024-02-14

--- src/main/java/com/movie/test/report/bookmark/controller/BookmarkController.java
+++ src/main/java/com/movie/test/report/bookmark/controller/BookmarkController.java
@@ -2,6 +2,9 @@ package com.movie.test.report.bookmark.controller;
 
 import com.movie.test.report.bookmark.dto.BookmarkDTO;
 import com.movie.test.report.bookmark.service.BookmarkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -21,8 +24,13 @@ public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
+    @Operation(summary = "북마크 리스트 조회", description = "북마크한 감상문을 조회합니다.")
+    @Parameters(value = {
+            @Parameter(name = "userId", description = "유저 아이디", required = true),
+            @Parameter(name = "bookmarkId", description = "마지막으로 조회된 북마크 아이디, 최초는 빈값"),
+    })
     @GetMapping("/list")
-    public ResponseEntity getBookmarkList(BookmarkDTO bookmarkDTO, Pageable pageable) {
+    public ResponseEntity getBookmarkList(BookmarkDTO bookmarkDTO, @Parameter(hidden = true) Pageable pageable) {
 
         Slice<BookmarkDTO> bookmarkList = bookmarkService.getBookmarkList(bookmarkDTO, pageable);
 
@@ -33,6 +41,11 @@ public class BookmarkController {
         return new ResponseEntity(returnData, HttpStatus.OK);
     }
 
+    @Operation(summary = "북마크 등록", description = "감상문을 북마크합니다.")
+    @Parameters(value = {
+            @Parameter(name = "userId", description = "유저 아이디", required = true),
+            @Parameter(name = "reportId", description = "북마크하려는 감상문 아이디", required = true),
+    })
     @PostMapping("/regist")
     public ResponseEntity registBookmark(BookmarkDTO bookmarkDTO) {
 
@@ -41,6 +54,10 @@ public class BookmarkController {
         return new ResponseEntity(savedBookmark, HttpStatus.OK);
     }
 
+    @Operation(summary = "북마크 삭제", description = "북마크한 감상문을 삭제합니다.")
+    @Parameters(value = {
+            @Parameter(name = "bookmarkId", description = "북마크 아이디", required = true),
+    })
     @DeleteMapping("/delete")
     public ResponseEntity deleteBookmark(Long bookmarkId){
 
