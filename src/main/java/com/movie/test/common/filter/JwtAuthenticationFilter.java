af9d3042d345219b9ed8d8b1c9ff7947525c0c96
K-CHANHO <cksgh1565@gmail.com>
2024-03-07 17:26:08 +0900

    Feat: Add log
    시큐리티 필터에 로그 추가
    
    by. chanho 2024-03-07

--- src/main/java/com/movie/test/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/movie/test/common/filter/JwtAuthenticationFilter.java
@@ -20,7 +20,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
 
         log.info("요청 URI : {}", request.getRequestURI());
         log.info("시큐리티 필터 진입");
@@ -31,6 +31,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
-        filterChain.doFilter(request, response);
+        log.info("시큐리티 필터 토큰 검증 성공");
+        try {
+            filterChain.doFilter(request, response);
+        } catch (IOException e) {
+            log.error("시큐리티 에러 : {}", e);
+            throw new RuntimeException(e);
+        } catch (ServletException e) {
+            log.error("시큐리티 에러 : {}", e);
+            throw new RuntimeException(e);
+        }
     }
 }
