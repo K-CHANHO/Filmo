86d1af34f3a7ff305d3b8330d5e21ef79bd741e4
Chanho Kim <cksgh1565@gmail.com>
2024-07-20 16:40:43 +0900

    Refactor: Json으로 리턴

--- src/main/generated/com/movie/test/user/userinfo/mapper/UserMapperImpl.java
+++ src/main/generated/com/movie/test/user/userinfo/mapper/UserMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-20T15:17:21+0900",
+    date = "2024-07-20T16:39:39+0900",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Oracle Corporation)"
 )
 public class UserMapperImpl implements UserMapper {
--- src/main/java/com/movie/test/user/userinfo/controller/UserController.java
+++ src/main/java/com/movie/test/user/userinfo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.movie.test.user.userinfo.controller;
 
+import com.google.gson.Gson;
 import com.movie.test.user.token.dto.JwtTokenDTO;
 import com.movie.test.user.token.service.TokenService;
 import com.movie.test.user.userinfo.dto.UserDto;
@@ -32,6 +33,7 @@ public class UserController {
 
     private final UserService userService;
     private final TokenService tokenService;
+    private final Gson gson;
 
     /**
      * 회원가입
@@ -43,25 +45,23 @@ public class UserController {
 
         UserDto newUser = userService.userSignup(userSignupDto);
 
-        return new ResponseEntity(newUser, HttpStatus.OK);
+        // TODO : JSON 변환 후 리턴
+        String jsonNewUser = gson.toJson(newUser);
+        return new ResponseEntity(jsonNewUser, HttpStatus.OK);
 
     }
 
     /**
      * 로그인
      */
-    @Operation(summary = "로그인 요청", description = "소셜로그인으로 부터 받은 정보로 로그인 처리 후 토큰을 발급합니다.")
-    @Parameters({
-            @Parameter(name = "uid", description = "소셜로그인으로부터 받은 uid", required = true),
-            @Parameter(name = "type", description = "소셜 플랫폼, kakao OR google OR naver", required = true)
-    })
+    @Operation(summary = "로그인 요청", description = "소셜로그인으로 부터 받은 정보로 로그인 처리 후 토큰을 발급합니다. 필요값 : uid, type")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "400", description = "회원일 경우 로그인 처리(토큰 발급)", headers = @Header(name = "Authorization", description = "토큰")),
+            @ApiResponse(responseCode = "200", description = "회원일 경우 로그인 처리(토큰 발급)", headers = @Header(name = "Authorization", description = "토큰")),
             @ApiResponse(responseCode = "401", description = "비회원일 경우 401 에러 리턴")
     })
     @PostMapping("/login")
     public ResponseEntity login(UserDto logingUser){
-
+        // TODO : 로그인 dto 추가 및 회원유무 서비스로 빼기.
         log.info("login 요청 : {}", logingUser.toString());
 
         UserDto getUserinfo = userService.getUserInfoByUidAndType(logingUser.getUid(), logingUser.getType());
