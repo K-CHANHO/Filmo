25700197bc760170de76f0a30980874502e041af
Chanho Kim <cksgh1565@gmail.com>
2024-09-25 00:12:43 +0900

    Refactor: 영화 검색 API에 page 추가

--- src/main/java/com/movie/test/common/api/tmdbAPI/dto/MovieSearchApiDTO.java
+++ src/main/java/com/movie/test/common/api/tmdbAPI/dto/MovieSearchApiDTO.java
@@ -1,6 +1,7 @@
 package com.movie.test.common.api.tmdbAPI.dto;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,14 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MovieSearchApiDTO {
 
-    // 검색어
+    @Schema(description = "검색어")
     private String query;
 
     @Hidden
     @Builder.Default
     private String language = "ko";
 
-    @Hidden
+    @Schema(description = "페이지", defaultValue = "1")
     @Builder.Default
     private int page = 1;
 }
