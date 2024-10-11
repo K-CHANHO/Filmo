4f5f1ee0f61c02c09232b29963a3f55466b33489
K-CHANHO <cksgh1565@gmail.com>
2024-10-11 11:46:50 +0900

    Mapper commit

--- src/main/generated/com/movie/test/inquiry/mapper/InquirySaveMapperImpl.java
+++ src/main/generated/com/movie/test/inquiry/mapper/InquirySaveMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-08T18:01:39+0900",
+    date = "2024-10-11T11:44:29+0900",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.3.jar, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class InquirySaveMapperImpl implements InquirySaveMapper {
